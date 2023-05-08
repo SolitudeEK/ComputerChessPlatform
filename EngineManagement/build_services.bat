fd6b95346158e37c7ba2411cc6d4053dfc410201
Knyazev,Egor <e.knyazev@student.fontys.nl>
2023-05-08 16:50:25 +0200

    Deleting game service(moved to separate branch) 	deleted:    EngineManagement/DockerfileEM 	deleted:    EngineManagement/EngineManagement.Proto/EngineManagement.Proto.csproj 	deleted:    EngineManagement/EngineManagement.Proto/GameManagement.cs 	deleted:    EngineManagement/EngineManagement.Proto/Proto/EngineManagement.proto 	deleted:    EngineManagement/EngineManagement.Proto/google/api/annotations.proto 	deleted:    EngineManagement/EngineManagement.Proto/google/api/http.proto 	deleted:    EngineManagement/EngineManagement.sln 	deleted:    EngineManagement/EngineManagement/Engine.cs 	deleted:    EngineManagement/EngineManagement/EngineManagement.csproj 	deleted:    EngineManagement/EngineManagement/IManager.cs 	deleted:    EngineManagement/EngineManagement/Manager.cs 	deleted:    EngineManagement/EngineStorage/EngineStorage.csproj 	deleted:    EngineManagement/EngineStorage/IStorage.cs 	deleted:    EngineManagement/EngineStorage/Storage.cs 	deleted:    EngineManagement/Host/Engines/komodo.exe 	deleted:    EngineManagement/Host/Engines/stockfish.exe 	deleted:    EngineManagement/Host/Host.csproj 	deleted:    EngineManagement/Host/Program.cs 	deleted:    EngineManagement/Host/Properties/launchSettings.json 	deleted:    EngineManagement/Host/Startup.cs 	deleted:    EngineManagement/Host/appsettings.json 	deleted:    EngineManagement/Host/appsettingsLinux.json 	deleted:    EngineManagement/Utils/FileLocation.cs 	deleted:    EngineManagement/Utils/SettingsHelpers.cs 	deleted:    EngineManagement/Utils/Utils.csproj 	deleted:    EngineManagement/build_services.bat

--- EngineManagement/build_services.bat
+++
@@ -1,9 +0,0 @@
-dotnet restore Host/Host.csproj
-
-dotnet publish Host/Host.csproj -c Release-Linux -r linux-x64 --output /bin/Release
-
-docker build -f .\DockerfileEM -t engine_managment .
-
-REM docker run -d -p 5000:5000 --name engine_managment engine_managment
-
-pause
